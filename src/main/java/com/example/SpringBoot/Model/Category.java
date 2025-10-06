b72f282883ab1968739b57cd66ef6a9bc0d30737
charan-crafts <mangalacharancharan@gmail.com>
2025-10-06 13:21:30 +0530

    add and created the categories

---
+++ src/main/java/com/example/SpringBoot/Model/Category.java
@@ -0,0 +1,30 @@
+package com.example.SpringBoot.Model;
+
+public class Category {
+
+    private String categoryName;
+
+    private long categoryId;
+
+    public Category(String categoryName){
+        long UUID = (long)(Math.random()*100000);
+        this.categoryId=UUID;
+        this.categoryName=categoryName;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(long categoryId) {
+        this.categoryId = categoryId;
+    }
+}
